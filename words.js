eccca98747afa0888d9ad810943cac7b4a1053af
tabine04 <binhku04@gmail.com>
2025-04-25 23:11:45 +0700

    Add files via upload

---
+++ words.js
@@ -0,0 +1,14 @@
+const words = [
+    { word: "Chair", phonetic: "/ʧɛər/", synonym: "Seat", antonym: "Stand" },
+    { word: "Table", phonetic: "/ˈteɪ.bəl/", synonym: "Desk", antonym: "Floor" },
+    { word: "Lamp", phonetic: "/læmp/", synonym: "Light", antonym: "Dark" },
+    { word: "Sofa", phonetic: "/ˈsəʊ.fə/", synonym: "Couch", antonym: "Stool" },
+    { word: "Curtain", phonetic: "/ˈkɜː.tən/", synonym: "Drape", antonym: "Window" },
+    { word: "Carpet", phonetic: "/ˈkɑː.pɪt/", synonym: "Rug", antonym: "Floor" },
+    { word: "Drawer", phonetic: "/drɔːr/", synonym: "Cabinet", antonym: "Shelf" },
+    { word: "Mirror", phonetic: "/ˈmɪr.ər/", synonym: "Glass", antonym: "Wall" },
+    { word: "Clock", phonetic: "/klɒk/", synonym: "Timer", antonym: "Timeless" },
+    { word: "Bed", phonetic: "/bed/", synonym: "Mattress", antonym: "Floor" },
+  ];
+  
+  export default words;
